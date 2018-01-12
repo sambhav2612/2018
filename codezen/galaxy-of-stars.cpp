c363ac260cf53b46bc3a826db1e936bd48b6b48b
Sambhav Jain <sambhavjain2612@gmail.com>
2018-01-12 21:43:27 +0530

    12/01/2018

---
+++ codezen/galaxy-of-stars.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main () {
+    system("clear");
+    int n;
+    cin >> n;
+
+    for (int i = 0; i <= n/2; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = n/2; i > 0; --i) {
+        for (int j = 0; j < i; ++j) {
+            cout <<"*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
