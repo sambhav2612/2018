767c17671bcb315c4bded6306cca2e52347dc6b8
Sambhav Jain <sambhavjain2612@gmail.com>
2018-01-26 18:21:17 +0530

    remove pdo

--- pdo/common.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    function escape() {
-        return htmlspecialchars($html, ENT_QUOTES | ENT_SUBSTITUTE, "UTF-8");
-    }
-
-?>
--- pdo/index.php
+++
@@ -1,8 +0,0 @@
-<?php include "templates/header.php"; ?>
-
-<ul>
-    <li><a href="create.php">Create</a> a user</li>
-    <li><a href="read.php">Read</a> a user</li>
-</ul>
-
-<?php include "templates/footer.php"; ?>
